9bcf5862bd881d9e639ec41413a1be9dbf63bb6a
SOMNATH VERMA <somverma.earth@outlook.com>
2025-11-10 03:08:49 -0500

    corrected env 2

--- module/azurerm_resource_group/variable.tf
+++ module/azurerm_resource_group/variable.tf
@@ -1,12 +1,11 @@
 variable "rgs" {
 
-      type = map(object({
-      name = string
-      location = string
-      managed_by = optional (string)
-      tags =  {
-        env = optional (map(string))
-       } 
-      }))
+  type = map(object({
+    name       = string
+    location   = string
+    managed_by = optional(string)
+    env        = optional(string)
+    tags       = optional(map(string))
+  }))
 }
        
